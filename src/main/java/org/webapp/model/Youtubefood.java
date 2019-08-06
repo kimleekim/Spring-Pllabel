5241e66e016dd4fd773434fe288b43adeb555f60
kym1229 <ymymym1229@gmail.com>
2019-08-06 15:28:38 +0900

    youtubefood, youtubehot VO update

---
+++ src/main/java/org/webapp/model/Youtubefood.java
@@ -0,0 +1,83 @@
+package org.webapp.model;
+
+import java.sql.Date;
+
+public class Youtubefood {
+    private long key;
+    private String station;
+    private String title;
+    private String creator;
+    private Date date;
+    private String thumbnailURL;
+    private String videoLink;
+
+    public Youtubefood (long key, String station, String title, String creator, Date date, String thumbnailURL, String videoLink) {
+        this.key = key;
+        this.station = station;
+        this.title = title;
+        this.creator = creator;
+        this.date = date;
+        this.thumbnailURL = thumbnailURL;
+        this.videoLink = videoLink;
+    }
+
+    public Youtubefood () {
+
+    }
+
+    public long getKey() {
+        return key;
+    }
+
+    public void setKey(long key) {
+        this.key = key;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+
+    public String getVideoLink() {
+        return videoLink;
+    }
+
+    public void setVideoLink(String videoLink) {
+        this.videoLink = videoLink;
+    }
+}
