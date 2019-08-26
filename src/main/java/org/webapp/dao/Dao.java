51e3fc7bedfd983e8ede334f2406cd974e922fcf
juhyeon <kjh96905@gmail.com>
2019-08-26 14:44:38 +0900

    edit Dao interface

--- src/main/java/org/webapp/dao/Dao.java
+++ src/main/java/org/webapp/dao/Dao.java
@@ -1,13 +1,14 @@
 package org.webapp.dao;
 
 import java.util.List;
+import java.util.Map;
 
 public interface Dao<T> {
     void save(T model);
 
-    List<T> findByParam(String column, Object parameter);
+    List<T> findByParam(Map<String, Object> parameter);
 
-    void delete(String column, Object parameter);
+    void delete(Map<String, Object> parameter);
 
     List<T> findAll();
 }
