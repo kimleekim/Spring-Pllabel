6de301392392c54dc4778a06608f3fd5d2137125
kym1229 <ymymym1229@gmail.com>
2019-08-26 12:26:37 +0900

    AWS S3 connecting

--- src/main/java/org/webapp/config/WebContextConfiguration.java
+++ src/main/java/org/webapp/config/WebContextConfiguration.java
@@ -3,6 +3,9 @@ package org.webapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -11,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @ComponentScan(basePackages={"org.webapp"})
 public class WebContextConfiguration implements WebMvcConfigurer {
+    private final int MAX_SIZE = 10 * 1024 * 1024;
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
@@ -18,5 +22,14 @@ public class WebContextConfiguration implements WebMvcConfigurer {
         viewResolver.setPrefix("WEB-INF/views/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(MAX_SIZE); // 10MB
+        multipartResolver.setMaxUploadSizePerFile(MAX_SIZE); // 10MB
+        multipartResolver.setMaxInMemorySize(0);
+        return multipartResolver;
     }
 }
