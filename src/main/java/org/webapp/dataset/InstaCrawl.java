f849f14eb6fca8cebb9471424392355f9caf4f0f
kym <ymymym1229@gmail.com>
2019-09-11 12:42:09 +0900

    add exception control to Instacrawl

--- src/main/java/org/webapp/dataset/InstaCrawl.java
+++ src/main/java/org/webapp/dataset/InstaCrawl.java
@@ -3,21 +3,25 @@ package org.webapp.dataset;
 import org.openqa.selenium.WebDriver;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 public interface InstaCrawl {
     public WebDriver setUpWebDriver(String searchKeyword, String station) throws Exception;
 
-    public String getDate(WebDriver driver);
+    public int waitPage(WebDriver driver, int position, int skipPosts);
+
+    public boolean checkPoint(int storedPosts);
+
+    public Date getDate(WebDriver driver) throws ParseException;
 
     public String getPost(WebDriver driver);
 
     public String getDescription(WebDriver driver, int index);
 
     public long getLikeCNT(WebDriver driver);
-
-    public boolean clickNextButton(WebDriver driver);
 
     public String getPhotoURL(WebDriver driver, String station, String url) throws IOException, Exception;
 
